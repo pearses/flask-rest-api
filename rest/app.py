3029c8df2aaa23a503a1a79207a23fcb53b5ffda
pearses <scott.pearse@dewc.com>
2023-07-13 03:31:54 +0000

    chore: update

--- rest/app.py
+++ rest/app.py
@@ -1,16 +1,17 @@
 import os
-from flask import Flask
+import secrets
+from flask import Flask, jsonify
 from flask_smorest import Api
-from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 
 from db import db
+from blocklist import BLOCKLIST
 import models
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
-
-
+from resources.user import blp as UserBlueprint
 
 
 # Factory Pattern
@@ -30,14 +31,67 @@ def create_app(db_url=None):
         "DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
+
     api = Api(app)
+
+    app.config["JWT_SECRET_KEY"] = "jose"
+    jwt = JWTManager(app)
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
     
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+            {"description": "The token has been revoked. ", "error": "token_revoked"}
+            ),
+            401,
+        )
+
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        # Add info to check database for admin credentials
+        if identity == 1:
+            return{"is_admin": True}
+        return {"is_admin": False}
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.",
+                    "error": "token_expired"}),
+            401,
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
+        )
+
     with app.app_context():
-            db.create_all()
+        db.create_all()
 
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(TagBlueprint)
-
+    api.register_blueprint(UserBlueprint)
 
     return app
